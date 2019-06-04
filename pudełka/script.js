0beb408ffc751f86e2433431bcfd8d9663c631d5
JaZa <47424360+JaZax@users.noreply.github.com>
2019-06-04 16:01:03 +0200

    challenge z reddita

---
+++ pudełka/script.js
@@ -0,0 +1,12 @@
+function fit1(X, Y, x, y)
+{
+
+    let xboxes = Math.floor(X / x); //haha xbox haha
+    let yboxes = Math.floor(Y / y); 
+    let result = xboxes * yboxes;
+
+    alert(result)
+
+}
+
+fit1(5, 100, 6, 1)
